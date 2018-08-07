8f7b8dcf858563bbe91a3025582f49fdd2fa055b
joannarogalska <rogalska.joasia@gmail.com>
2018-08-07 12:33:49 +0200

    Obsłużenie pozostałych endpointów - css | Task 11.6

--- client/modules/Lane/Lanes.js
+++ client/modules/Lane/Lanes.js
@@ -1,10 +1,17 @@
 import React, { PropTypes } from 'react';
 import Lane from './LaneContainer.js';
 
+// Import Style
+import styles from './Lane.css';
+
 const Lanes = ({ lanes }) => {
+  console.log('lanes',lanes.length);
   return (
-    <div className="lanes">{lanes.map(lane =>
-      <Lane key={lane.id} lane={lane} />
+    <div className="lanes">{lanes.map((lane, index) =>
+      <div key={index}>
+        <Lane key={lane.id} lane={lane} />
+        {(!((index+1) % 4)) && <div className={styles.clear}></div>}
+      </div>
     )}</div>
   );
 };
